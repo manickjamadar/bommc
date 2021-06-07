da05b71d70cae85b52b2f696ece4583c8f66e348
creanick <creanickofficial@gmail.com>
2021-06-07 18:46:44 +0530

    selection of chain size added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,40 +7,49 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class HomeScreen extends StatelessWidget {
   buildAmountChip(BuildContext context, String amount, int position,
       String currencySymbol) {
-    return Container(
-        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(6),
-            border:
-                Border.all(color: Theme.of(context).primaryColor, width: 1)),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              child: Center(
-                  child: Text(
-                position.toString(),
-                style: TextStyle(color: Colors.white),
-              )),
-              padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(3),
-                color: Theme.of(context).primaryColor,
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Text(
-              "$currencySymbol $amount",
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                  color: Theme.of(context).primaryColor),
-            ),
-          ],
-        ));
+    return InkWell(onTap: () {
+      BlocProvider.of<cubit.FormCubit>(context).selectAmount(position);
+    }, child: BlocBuilder<cubit.FormCubit, cubit.FormState>(
+      builder: (context, state) {
+        final activeColor = Theme.of(context).primaryColor;
+        final disabledColor = Colors.grey;
+        final isEnabled = position <= state.selectedChainSize;
+        final chipColor = isEnabled ? activeColor : disabledColor;
+        return Container(
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                border: Border.all(color: chipColor, width: 1)),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: Center(
+                      child: Text(
+                    position.toString(),
+                    style: TextStyle(color: Colors.white),
+                  )),
+                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3),
+                    color: chipColor,
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "$currencySymbol $amount",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: chipColor),
+                ),
+              ],
+            ));
+      },
+    ));
   }
 
   @override
